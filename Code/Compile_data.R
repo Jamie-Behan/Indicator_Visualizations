861c837e7a6c71fa9a90ea57d3bbaf748ab62cda
Jamie-Behan <jamie.behan@maine.edu>
2023-10-12 14:40:26 -0400

    creating new scripts to help clean up code in app.R

---
+++ Code/Compile_data.R
@@ -0,0 +1,30 @@
+########## ENVIRONMENTAL DATA###################################################################################
+#### SOURCE "automatic_ecodata.R"
+source(here("Code/automatic_ecodata.R"))
+big_ecodata<-lapply(ecodata_df,as.numeric)
+####load salinity data from GLORYs####
+salinity_data<-read.csv(here("Data/GLORYs_salinity/mean_salinity_GLORYs.csv"))
+######### FISH DATA ###########################################################################################
+####load in stock data from stock assessment reports####
+stock_assess_data<-read.csv(here("Data/stock_assess_data2.csv"))
+stock_assess_data<-replace(stock_assess_data,stock_assess_data=='',NA)
+stock_assess_data<-sapply(stock_assess_data,as.character)
+stock_assess_data<- as.data.frame(gsub(",","",stock_assess_data))
+stock_assess_data<-lapply(stock_assess_data,as.numeric)
+####### load in DisMAP data######
+source(here("Code/Dismap_data.R"))
+#################################### Merge all dfs ##############################################################
+Both <- list(big_ecodata, salinity_data,stock_assess_data,DisMAPdata)
+Both <-Reduce(function(x, y) merge(x, y, by="Year",all=T), Both)
+Both <-Both[rowSums(is.na(Both)) != ncol(Both), ] #removes rows containing all NAs
+
+uniquecolnames <- unique(gsub("_[^_]+$", "", names(Both)[-1])) #get unique column names excluding year
+
+#Both2<-as.data.frame(big_ecodata)
+Both2<-as.data.frame(Both[c(1:19,31:34)]) #dismap herring data is being treated as ENV var and lobster data as fish data
+Both2<-Both2[ , order(names(Both2))]
+Both2 <- list(Both[1],Both2, stock_assess_data,DisMAPdata[c(1,2,3,8,9)])
+Both2 <-Reduce(function(x, y) merge(x, y, by="Year",all=T), Both2)
+Both2 <-Both2[rowSums(is.na(Both2)) != ncol(Both2), ] #removes rows containing all NAs
+#the Both2 df is the same as "Both", but is organized in alphabetical order
+rm(big_ecodata,DisMAPdata,ecodata_df,salinity_data,stock_assess_data)
--- Code/sea_grant_app.R
+++ Code/sea_grant_app.R
@@ -1,119 +1,13 @@
-#### Species Tab test ######
-
 #### Load R packages ######
 if (!require("pacman")) install.packages("pacman")
 library(pacman)
 pacman::p_load(shiny,plotly,DT,shinyjs,shinythemes,dplyr,stringr,readtext,XML,data.table,ecodata,shinyBS,huxtable,gridExtra,ggplot2,shinyWidgets,readxl,htmltools,knitr,shinydashboard,shinydashboardPlus,gmRi,here,install=TRUE)
 here()
-#### SOURCE "automatic_ecodata.R"
-source(here("Code/automatic_ecodata.R"))
-big_ecodata<-lapply(ecodata_df,as.numeric)
-####load ecodata that has been combined into one big df from "making_big_ecodata_df.R"####
-stock_assess_data<-read.csv(here("Data/stock_assess_data2.csv"))
-stock_assess_data<-replace(stock_assess_data,stock_assess_data=='',NA)
-stock_assess_data<-sapply(stock_assess_data,as.character)
-stock_assess_data<- as.data.frame(gsub(",","",stock_assess_data))
-stock_assess_data<-lapply(stock_assess_data,as.numeric)
-####load salinity data from GLORYs####
-salinity_data<-read.csv(here("Data/GLORYs_salinity/mean_salinity_GLORYs.csv"))
-##Merge all dfs
-Both <- list(big_ecodata, salinity_data,stock_assess_data)
-Both <-Reduce(function(x, y) merge(x, y, by="Year",all=T), Both)
-Both <-Both[rowSums(is.na(Both)) != ncol(Both), ] #removes rows containing all NAs
+##### LOAD DATA ######
+source(here("Code/Compile_data.R"))
+##### LOAD supporting functions ########
+source(here("Code/supporting_functions_for_app.R"))
 
-uniquecolnames <- unique(gsub("_[^_]+$", "", names(Both)[-1])) #get unique column names excluding year
-
-Both2<-as.data.frame(big_ecodata)
-Both2<-Both2[ , order(names(Both2))]
-Both2 <- list(Both[1],Both2,salinity_data, stock_assess_data)
-Both2 <-Reduce(function(x, y) merge(x, y, by="Year",all=T), Both2)
-Both2 <-Both2[rowSums(is.na(Both2)) != ncol(Both2), ] #removes rows containing all NAs
-
-###### tweaks, a list object to set up multicols for checkboxGroupInput#####
-tweaks <- 
-  list(tags$head(tags$style(HTML("
-                                 .multicol { 
-                                   height: 300px;
-                                   -webkit-column-count: 2; /* Chrome, Safari, Opera */ 
-                                   -moz-column-count: 2;    /* Firefox */ 
-                                   column-count: 2; 
-                                   -moz-column-fill: auto;
-                                   -column-fill: auto;
-                                 } 
-                                 ")) 
-  ))
-##### data control: the checkboxes will control the data values plotted ######
-controls <-
-  list(h3("Select Environmental Variables"), 
-       tags$div(align = 'left', 
-                class = 'multicol', 
-                checkboxGroupInput("variable", 
-                                   label = NULL, 
-                                   choiceNames  = sort(gsub("_", " ", uniquecolnames[1:18]),decreasing = FALSE), ##replace_with space and select columns on env data only
-                                   
-                                   choiceValues = sort(colnames(Both[2:19]),decreasing = FALSE),  ##select columns on env data only
-                                   selected = c("Bottom_Temp_Anomaly_GOM",multiple = TRUE)) 
-                
-       )#close tags$div
-  )#close list 
-##### controls2: year column included######
-controls2 <-
-  list(h3("Select Environmental Variables"), 
-       tags$div(align = 'left', 
-                class = 'multicol', 
-                checkboxGroupInput("show_vars", 
-                                   label = NULL, 
-                                   choiceNames  = gsub("_", " ", colnames(Both2[1:19])), ##select columns on env data only
-                                   
-                                   choiceValues = colnames(Both2[1:19]), ##select columns on env data only 
-                                   selected = c("Year","Bottom_Temp_Anomaly_GOM",multiple = TRUE))
-       )#close tags$div
-  )#close list 
-###### finding name of second longest column (excluding NAs)
-second_col<- function (df){
-  m1 = sapply(df, function(x) sum(!is.na(x))) #find length of each column
-  m2 = m1[-which.max(m1)] #find second longest column
-  mN = names(which.max(m2)) #get name of second longest column
-  mP<-match(mN,names(df)) #match name of second longest column to column name of df
-  mL = length(na.omit(df[,mP])) #reomve NAs of identified column of df and get length
-  return(mL)
-}
-
-########create sidebar: #########
-sidebar <- dashboardSidebar(width = 150,
-                            tags$style(".left-side, .main-sidebar {padding-top: 100px}"),
-                            sidebarMenu(
-                              menuItem("Home", tabName = "Home", icon = icon("house")),
-                              menuItem("Striped Bass", tabName = "StripedBass", icon = icon("fish")),
-                              menuItem("Bluefin Tuna", tabName = "BluefinTuna",icon = icon("fish")),
-                              menuItem("American Lobster", tabName = "AmericanLobster",icon = icon("fish")),
-                              menuItem("Metadata", tabName = "Metadata",icon = icon("list"))
-                              
-                            ),
-                            HTML(paste0(
-                              "<div style='margin-top: 100px; text-align: center;'>",
-                              "<a href='https://github.com/Jamie-Behan' target='_blank'>", 
-                              icon("github", class = "fa-3x"), 
-                              "<br/>Jamie Behan 2023",
-                              "</a>",
-                              "</div>"
-                            ))
-)
-######## define colors#####
-gmri_colors<-tags$head(tags$style(HTML('
-        /* main sidebar */
-        .skin-blue .main-sidebar {
-        background-color: #00608A;
-        }
-        /* active selected tab in the sidebarmenu */
-        .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
-        background-color: #00736D;
-        }
-        /* body */
-        .content-wrapper, .right-side {
-        background-color: #E9E9E9;
-        }
-        ')))
 ##########################
 ###### Define UI ######
 ui <- dashboardPage(
